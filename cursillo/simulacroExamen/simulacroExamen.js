00503a4b3fe66e0e275907f3957d9321a1aac637
Daiana <dayi.aguero@gmail.com>
2024-03-26 21:16:24 -0300

    terceraParte

--- cursillo/simulacroExamen/simulacroExamen.js
+++ cursillo/simulacroExamen/simulacroExamen.js
@@ -1,11 +1,16 @@
-function condicionAlunmo (nota) {
-    if (nota <= 4){
-        console.log('LIBRE');
-    } else if (nota > 4 && nota < 7) {
-        console.log('LIBRE-REGULAR');
-    } else {
-        console.log('PROMOCIONADO');
-    }
+function condicionAlunmo(nota1, nota2, nota3) {
+    let promedio = ((nota1+nota2+nota3)/3);
+    let nota = promedio;
+
+  if (nota >= 1 && nota <= 4) {
+    console.log("LIBRE");
+  } else if (nota > 4 && nota < 7) {
+    console.log("LIBRE-REGULAR");
+  } else if (nota > 6 && nota <= 10) {
+    console.log("PROMOCIONADO");
+  } else  {
+  console.log("ERROR, Inserte una nota del 1 al 10");
+  }
 }
 
-condicionAlunmo(7);
+condicionAlunmo(0,0,0);
